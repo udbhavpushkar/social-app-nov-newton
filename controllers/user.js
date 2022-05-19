7d524c341e194b2a017fb8635632571d376bc7a0
udbhavpushkar <udbhavpushkar@gmail.com>
2022-05-19 22:48:41 +0530

    auth

--- controllers/user.js
+++ controllers/user.js
@@ -1,9 +1,72 @@
 const User = require("../models/user")
+const bcrypt = require("bcryptjs")
 
 const registerUser = async (req, res)=>{
-    const data = req.body
-    console.log(data);
-    res.json({message: "User is registered"})
+    //Get user detail from request body and store it in varibles
+    const {name, email, password} = req.body
+    
+    try{
+        //check fields are not empty
+        if(!name || !email || !password){
+            throw "Please fill the fields"
+        }
+
+        //find the email in database and if it exsist throw error => user alsreay exsists
+        const userAlready = await User.findOne({email})
+        if (userAlready) {
+            throw "User already exsists"
+        }
+
+        //create user and save in database
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const user = new User({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const savedUser = await user.save()
+
+        res.json({
+            _id: savedUser.id,
+            name: savedUser.name,
+            email: savedUser.email
+        })
+
+
+    }catch(e){
+        res.status(400).json({error: e})
+    }
 }
 
-module.exports = {registerUser}
+const loginUser = async (req, res)=>{
+    //get credentials from request body
+    const {email, password} = req.body
+
+    
+    try{
+        //find user with that email
+        const user = await User.findOne({email})
+        //compare password with the stored hashed password
+        if(user && (await bcrypt.compare(password, user.password))){
+            res.json({
+                _id: user.id,
+                name: user.name,
+                email: user.email
+            })
+        }else{
+            throw "Wrong credentials"
+        }
+
+    }catch(e){
+        res.status(400).json({error: e})
+    }
+
+        
+
+    
+}
+
+module.exports = {registerUser, loginUser}
--- models/post.js
+++ models/post.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose")
 
 const postSchema = mongoose.Schema({
     description: {type: String, required: true},
-    vote_count : {type: Number, default: 0}
+    vote_count : {type: Number, default: 0},
+    owner : {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'User'}
 },
 {timestamps: true}
 )
